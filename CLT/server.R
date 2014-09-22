f2f4b9c1b11e54f881495067250e0f8c3787bbf7
Dillon Niederhut <dillon.niederhut@gmail.com>
2014-09-22 14:36:38 -0700

    CLT printing updating error fixed

--- CLT/server.R
+++ CLT/server.R
@@ -2,13 +2,18 @@ library(shiny)
 library(ggplot2)
 
 shinyServer(function(input, output) {
+ 
+  normal <- rnorm(n = 10000, mean = 0, sd = 1)
+  uniform <- runif(n = 10000, min = -3, max = 3)
+  geometric <- rgeom(n = 10000, prob = 0.5)
+  exponential <- rexp(n = 10000, rate = 1)
+  dist <- data.frame(norm = rnorm(n=10000, mean = 0, sd = 1))
   
   output$distribution <- renderPlot({ 
-    if (input$distro == 'normal') dist <- data.frame(x = rnorm(n = 10000, mean = 0, sd = 1))
-    if (input$distro == 'uniform') dist <- data.frame(x = runif(n = 10000, min = -3, max = 3))
-    if (input$distro == 'geometric') dist <- data.frame(x = rgeom(n = 10000, prob = 0.5))
-    if (input$distro == 'exponential') dist <- data.frame(x = rexp(n = 10000, rate = 1))
-    dist$norm <- rnorm(n=10000, mean = 0, sd = 1)
+    if (input$distro == 'normal') dist$x <- normal
+    if (input$distro == 'uniform') dist$x <- uniform
+    if (input$distro == 'geometric') dist$x <- geometric
+    if (input$distro == 'exponential') dist$x <- exponential
     title1 <- paste0("Comparison of the ", input$distro, " and normal curves (dashed red line)")
     plot1 <- ggplot(dist) + geom_density(aes(x = x)) + 
              geom_density(aes(x = norm),colour = 'red', linetype = 'dashed') +  
@@ -16,11 +21,15 @@ shinyServer(function(input, output) {
     print(plot1)
   })
   
-  output$sampling <- renderPlot({ ##failing to update dist$x
+  output$sampling <- renderPlot({
+    if (input$distro == 'normal') dist$x <- normal
+    if (input$distro == 'uniform') dist$x <- uniform
+    if (input$distro == 'geometric') dist$x <- geometric
+    if (input$distro == 'exponential') dist$x <- exponential  
     dist$sam <- replicate(1000, mean(sample(dist$x, size = input$n, replace = TRUE)))
     title2 <- paste0("Distribution of sample means at size: ", input$n)
     plot2 <- ggplot(dist) + geom_density(aes(x = sam)) +  
       ggtitle(title2) + xlab("Values") + ylab("Density")
-    print(plot2)
+    print(plot2) 
   })
 })
