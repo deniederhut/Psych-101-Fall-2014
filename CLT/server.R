1090792c82fa96b2f8fc611add8dc943cc12a3c3
Dillon Niederhut <dillon.niederhut@gmail.com>
2014-09-22 10:17:34 -0700

    Split UI into two graphs for clarity; second graph failing to update dist variable

--- CLT/server.R
+++ CLT/server.R
@@ -3,18 +3,24 @@ library(ggplot2)
 
 shinyServer(function(input, output) {
   
-  output$distribution <- renderPlot({
-    if (input$distro == 'norm') dist <- data.frame(x = rnorm(n = 10000, mean = 0, sd = 1))
-    if (input$distro == 'unif') dist <- data.frame(x = runif(n = 10000, min = -3, max = 3))
-    if (input$distro == 'geo') dist <- data.frame(x = rgeom(n = 10000, prob = 0.5) - 1)
-    if (input$distro == 'exp') dist <- data.frame(x = rexp(n = 10000, rate = 1) - 1)
+  output$distribution <- renderPlot({ 
+    if (input$distro == 'normal') dist <- data.frame(x = rnorm(n = 10000, mean = 0, sd = 1))
+    if (input$distro == 'uniform') dist <- data.frame(x = runif(n = 10000, min = -3, max = 3))
+    if (input$distro == 'geometric') dist <- data.frame(x = rgeom(n = 10000, prob = 0.5))
+    if (input$distro == 'exponential') dist <- data.frame(x = rexp(n = 10000, rate = 1))
     dist$norm <- rnorm(n=10000, mean = 0, sd = 1)
-    dist$sam <- replicate(1000, mean(sample(dist$x, size = input$n, replace = TRUE)))
-    plot1 <- ggplot(dist) + geom_density(aes(x = norm), linetype = 'dashed') + 
-             geom_density(aes(x = x),colour = 'red') + 
-             geom_density(aes(x = sam), colour = 'blue') + 
-             ggtitle("Comparison of the population, sampling distribution, and normal curve") + 
-             xlab("Values") + ylab("Density")
+    title1 <- paste0("Comparison of the ", input$distro, " and normal curves (dashed red line)")
+    plot1 <- ggplot(dist) + geom_density(aes(x = x)) + 
+             geom_density(aes(x = norm),colour = 'red', linetype = 'dashed') +  
+             ggtitle(title1) + xlab("Values") + ylab("Density")
     print(plot1)
+  })
+  
+  output$sampling <- renderPlot({ ##failing to update dist$x
+    dist$sam <- replicate(1000, mean(sample(dist$x, size = input$n, replace = TRUE)))
+    title2 <- paste0("Distribution of sample means at size: ", input$n)
+    plot2 <- ggplot(dist) + geom_density(aes(x = sam)) +  
+      ggtitle(title2) + xlab("Values") + ylab("Density")
+    print(plot2)
   })
 })
--- CLT/ui.R
+++ CLT/ui.R
@@ -11,16 +11,16 @@ shinyUI(fluidPage(
       p("Choose different options for population distributions and sample sizes to see
        what happens to your distribution of sample means"),
       radioButtons("distro", label = "Poplation distribution",
-                   choices = c("Normal" = "norm",
-                               "Uniform" = "unif",
-                               "Geometric" = "geo",
-                               "Exponential" = "exp")),
-      numericInput("n", label = "Sample size", value = 10, min = 1, max = 100)),
+                   choices = c("Normal" = "normal",
+                               "Uniform" = "uniform",
+                               "Geometric" = "geometric",
+                               "Exponential" = "exponential")),
+      numericInput("n", label = "Sample size", value = 1, min = 1, max = 100)),
     mainPanel(
       p("The population distribution and distribution of sample means 
-        produced by your options appears below, with a normal curve
-        superimposed on top"),
-      plotOutput("distribution", hoverId = "Original distribution")),
+        produced by your options appear below"),
+      plotOutput("distribution", hoverId = "Original distribution"),
+      plotOutput("sampling", hoverId = "Distribution of sample means")),
     position = 'right'
   )
 ))
